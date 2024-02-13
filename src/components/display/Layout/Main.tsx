980dd823c6d2fe3b07e962b44a4a015757066c55
kkuyu <devils67@naver.com>
2024-02-13 14:54:54 +0900

    Design: Layout (#16)
    Add Components
    - display/Layout
    
    Add Libs
    - timer
    - hook/useFocusTrap
    - hook/useResize
    
    Add Stores
    - global
    
    Add Image
    - logo
    
    # Issues: 9

---
+++ src/components/display/Layout/Main.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useEffect, useMemo } from "react";
+import { useRecoilState } from "recoil";
+import { styled } from "styled-components";
+import { ObjectEntries } from "@/libs/utils";
+import useResize from "@/libs/hook/useResize";
+import { atomGlobal } from "@/stores/global";
+import { ScreenSize, screenSize } from "@/styles/theme/screen";
+
+export interface LayoutMainProps
+  extends React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>> {
+  //
+}
+
+const LayoutMain = (props: LayoutMainProps) => {
+  const { className = "", children, ...restProps } = props;
+
+  const { resizeStructure } = useResize();
+  const [global, setGlobal] = useRecoilState(atomGlobal);
+
+  const currentScreen = useMemo<(keyof ScreenSize)[]>(() => {
+    const result = [] as (keyof ScreenSize)[];
+    const sizes = Object.entries(screenSize) as ObjectEntries<
+      typeof screenSize
+    >;
+    sizes.sort(([, a], [, b]) => parseInt(a) - parseInt(b));
+    if (resizeStructure.windowInnerWidth === 0) {
+      return result;
+    }
+    for (const [key, value] of sizes) {
+      result.push(key);
+      if (resizeStructure.windowInnerWidth <= parseInt(value)) break;
+    }
+    return result;
+  }, [resizeStructure.windowInnerWidth]);
+
+  useEffect(() => {
+    if (JSON.stringify(global.screen) !== JSON.stringify(currentScreen)) {
+      setGlobal((prev) => ({
+        ...prev,
+        screen: currentScreen,
+      }));
+    }
+  }, [currentScreen, global.screen, setGlobal]);
+
+  return (
+    <LayoutMainContainer className={`${className}`} {...restProps}>
+      {children}
+    </LayoutMainContainer>
+  );
+};
+
+const LayoutMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export default LayoutMain;
---
+++ src/components/display/Layout/index.ts
@@ -0,0 +1,23 @@
+import LayoutMain, { LayoutMainProps } from "@/components/display/Layout/Main";
+import LayoutHeader, {
+  LayoutHeaderProps,
+} from "@/components/display/Layout/Header";
+import LayoutContent, {
+  LayoutContentProps,
+} from "@/components/display/Layout/Content";
+import LayoutFooter, {
+  LayoutFooterProps,
+} from "@/components/display/Layout/Footer";
+
+export type {
+  LayoutMainProps,
+  LayoutHeaderProps,
+  LayoutContentProps,
+  LayoutFooterProps,
+};
+
+export default Object.assign(LayoutMain, {
+  Header: LayoutHeader,
+  Content: LayoutContent,
+  Footer: LayoutFooter,
+});
