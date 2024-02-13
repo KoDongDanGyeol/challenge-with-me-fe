66296c3c3cb5b2b6e490129606e47c6886deebce
kkuyu <devils67@naver.com>
2024-02-13 13:35:26 +0900

    Design: Icon (Bars, XMark) #11 (#15)
    * Design: Icon
    
    Add Components
    - general/Icon/data/Bars
    - general/Icon/data/XMark
    
    # Issues: 11

--- src/components/general/Icon/data/type.ts
+++ src/components/general/Icon/data/type.ts
@@ -1,6 +1,8 @@
 export const IconName = {
+  ["Bars"]: "Bars",
   ["SymbolGithub"]: "SymbolGithub",
   ["SymbolGoogle"]: "SymbolGoogle",
+  ["XMark"]: "XMark",
 } as const;
 
 export type IconName = (typeof IconName)[keyof typeof IconName];
--- src/components/general/Icon/index.tsx
+++ src/components/general/Icon/index.tsx
@@ -3,8 +3,7 @@ import { IconName } from "@/components/general/Icon/data/type";
 import IconMain from "@/components/general/Icon/Main";
 
 export const Icons = data;
+
 export type { IconName };
 
-export default Object.assign(IconMain, {
-  //
-});
+export default IconMain;
