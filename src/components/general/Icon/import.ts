435ebe9eacf22e501db84c276baae018ff2bc4a6
kkuyu <devils67@naver.com>
2024-02-15 23:17:29 +0900

    Revert "Feature: ChallengeIDE (#25)" (#27)
    This reverts commit 370c2e3f7bfcb2f87b38c3e84224926f50392320.

--- src/components/general/Icon/import.ts
+++ src/components/general/Icon/import.ts
@@ -1,6 +1,4 @@
 export * from "@/components/general/Icon/data/Bars"
-export * from "@/components/general/Icon/data/Loading"
 export * from "@/components/general/Icon/data/SymbolGithub"
 export * from "@/components/general/Icon/data/SymbolGoogle"
-export * from "@/components/general/Icon/data/XCircle"
 export * from "@/components/general/Icon/data/XMark"
--- src/components/general/Icon/type.ts
+++ src/components/general/Icon/type.ts
@@ -1,9 +1,7 @@
 export const IconName = {
   ["Bars"]: "Bars",
-  ["Loading"]: "Loading",
   ["SymbolGithub"]: "SymbolGithub",
   ["SymbolGoogle"]: "SymbolGoogle",
-  ["XCircle"]: "XCircle",
   ["XMark"]: "XMark",
 } as const
 
