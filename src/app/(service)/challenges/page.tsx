f4fd699604e01c12a1d4fe6d1c53ec1d2abf25f2
kkuyu <devils67@naver.com>
2024-02-20 13:31:59 +0900

    Feature: ChallengeHome (#42)
    Update Pages
    
    - (service)/challenges/_components/display/ChallengeHome
    - (service)/challenges/page
    
    Update Components
    - /display/ChallengeList
    - /entry/Input
    - /entry/Select
    - /form/ChallengeFilter
    - /general/Icon
    
    Issues: #29

---
+++ src/app/(service)/challenges/page.tsx
@@ -0,0 +1,7 @@
+import ChallengeHome from "@/app/(service)/challenges/_components/display/ChallengeHome"
+
+const page = () => {
+  return <ChallengeHome asTag="section" className="container" />
+}
+
+export default page
