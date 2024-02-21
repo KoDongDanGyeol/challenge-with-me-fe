17efa7d246048e96c93f61a7f6cc58d0dc664309
kkuyu <devils67@naver.com>
2024-02-21 15:06:55 +0900

    Feature: Picture (#48)
    Update Components
    - display/Picture
    
    Issues: #47

---
+++ src/components/display/Picture/index.ts
@@ -0,0 +1,8 @@
+import { PictureRounded } from "@/components/display/Picture/type"
+import PictureMain, { PictureMainProps } from "@/components/display/Picture/Main"
+
+export type { PictureRounded, PictureMainProps }
+
+export default Object.assign(PictureMain, {
+  //
+})
