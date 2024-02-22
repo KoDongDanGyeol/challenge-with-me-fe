35fa043ea7231c27e9620de4fd91ac71727cf61f
kkuyu <devils67@naver.com>
2024-02-22 23:54:41 +0900

    Core!: ReDeploy
    Fix Build Error
    
    Issues: #59

--- src/app/(service)/challenges/[problemId]/questions/page.tsx
+++ src/app/(service)/challenges/[problemId]/questions/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import ChallengeQuestionHome from "@/app/(service)/challenges/_components/display/ChallengeQuestionHome"
 
 const page = () => {
-  return <ChallengeQuestionHome asTag="section" className="container" />
+  return (
+    <Suspense>
+      <ChallengeQuestionHome asTag="section" className="container" />
+    </Suspense>
+  )
 }
 
 export default page
--- src/app/(service)/questions/page.tsx
+++ src/app/(service)/questions/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import QuestionHome from "@/app/(service)/questions/_components/display/QuestionHome"
 
 const page = () => {
-  return <QuestionHome asTag="section" className="container" />
+  return (
+    <Suspense>
+      <QuestionHome asTag="section" className="container" />
+    </Suspense>
+  )
 }
 
 export default page
