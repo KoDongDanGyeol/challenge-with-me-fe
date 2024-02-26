f8a3b26a05b7aa7b2d8cfa40f8e210de9109f1ed
Jaewon <grayhound0801@gmail.com>
2024-02-26 09:30:58 +0900

    Feature : signIn & myPage api success

--- next.config.js
+++ next.config.js
@@ -16,10 +16,26 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/:path*`,
-      },
+      // {
+      //   source: "/api/auth/session",
+      //   destination: "/api/auth/session",
+      // },
+      // {
+      //   source: "/api/auth/log",
+      //   destination: "/api/auth/log",
+      // },
+      // {
+      //   source: "/api/auth/signin",
+      //   destination: "/api/auth/signin",
+      // },
+      // {
+      //   source: "/api/auth/signout",
+      //   destination: "/api/auth/signout",
+      // },
+      // {
+      //   source: "/api/:path*",
+      //   destination: `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/:path*`,
+      // },
     ]
   },
 }
